6fbc466f1e7c6dacaab11db9755322c837a48fd8
polya <pstartceva@yandex.ru>
2025-09-20 11:09:57 +0300

    change file

---
+++ src/Range.kt
@@ -0,0 +1,15 @@
+fun main() {
+//    val numbers = 1..100
+//    val symbol = 'a'..'z'
+//    print("Введите ваш уровень: ")
+//    val level = readln().toInt()
+//    if (level in 1..50) {
+//        println("Уровень $level соответствует требованиям! Вы можете зайти в данж")
+//    } else {
+//        println("Высокий уровень! Вход запрещен!")
+//    }
+    val symbols = 'z' downTo 'a'
+    val numbers = 5 until 11
+    val numbers2 = 10..50 step 3
+
+}
--- src/lab2.kt
+++ src/lab2.kt
@@ -1,5 +1,32 @@
 fun main() {
-    val number = 10
-    println(number)
-    println()
+//    println("введите первое число:")
+//    val num = readln().toInt()
+//    println("введите второе число:")
+//    val num2 = readln().toInt()
+//    if (num>num2) {
+//        println("Первое число больше")
+//    } else {
+//        println("Второе число больше")
+//    }
+//    val max = if (num>num2) num else num2
+//    println("Максимальное число: $max")
+//    val age = readln().toInt()
+//    val status = if (age>=18) "Совершеннолетний" else "Несовершеннолетний"
+//    println(status)
+//    print("Введите урон: ")
+//    var damage = readln().toInt()
+//    print("Щит активен? (введите да или нет): ")
+//    val answer = readln()
+//    val dam = if (answer == "да") damage/2 else damage
+//    println("Полученный урон: $dam")
+    print("Введите наш уровень: ")
+    val level = readln().toInt()
+    print("У вас есть VIP-cтатус? (true/false): ")
+    val hasvip = readln().toBoolean()
+    if ((level >= 30 && hasvip) || level >= 50) {
+        println("Доступ разрешен!")
+    } else {
+        println("Доступ запрещен!")
+    }
+
 }
